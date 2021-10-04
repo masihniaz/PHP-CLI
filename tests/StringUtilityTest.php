8ab44f91892a299d5832091e64dd52f1f5940bab
Masih Niaz <masihniaz@yahoo.com>
2021-10-04 15:59:37 +0800

    Added setUp for tests

--- tests/StringUtilityTest.php
+++ tests/StringUtilityTest.php
@@ -6,28 +6,30 @@ include "src/StringUtility.php";
 
 class StringUtilityTest extends TestCase
 {
+  protected $string;
+
+  protected function setUp(): void {
+    $this->string = "hello world";
+  }
 
   public function testToUpperCase()
   {
-    $string = "hello world";
     $expected = "HELLO WORLD";
-    $converted = StringUtility::toUpperCase($string);
+    $converted = StringUtility::toUpperCase($this->string);
     $this->assertSame($expected, $converted);
   }
 
   public function testToAlternateUpperCase()
   {
-    $string = "hello world";
     $expected = "hElLo wOrLd";
-    $converted = StringUtility::toAlternateUpperCase($string);
+    $converted = StringUtility::toAlternateUpperCase($this->string);
     $this->assertSame($expected, $converted);
   }
 
   public function testConvertToCsv()
   {
-    $string = "hello world";
     $expected = "h,e,l,l,o, ,w,o,r,l,d";
-    $csv = StringUtility::convertToCsv($string);
+    $csv = StringUtility::convertToCsv($this->string);
     $this->assertSame($expected, $csv);
   }
 
